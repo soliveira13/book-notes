d83a178ad0cdb3341f5b637a94eb348288fe97e9
Sergio De Oliveira <sergiooliveira13@hotmail.com>
2023-12-17 13:15:40 -0300

    Initial Commit - Documentation: ERD Diagram, Wireframes and Flowchart - Home Page with manual data

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
